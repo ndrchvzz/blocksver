9af2e2fb7f95f2c4b7b68b8f8fb624a18d1a33c7
Andrea Chiavazza <ndrchvzz@gmail.com>
2016-11-06 22:56:00 +0000

    Test that cached data is used, and reindentings

--- blocksver.py
+++ blocksver.py
@@ -282,72 +282,78 @@ def test_updateCache():
     # 3 new blocks from an empty cache
     cache = Cache((), (), 0, {})
     cache = updateCache(cache, window, hashesSize, 'h03', 38, testRetriever)
-    assertEquals(cache, Cache((3, 3, 1),          ('h03', 'h02', 'h01'), 38, { 3:2, 1:1 }))
+    assertEquals(cache, Cache(         (3, 3, 1,), ('h03', 'h02', 'h01'), 38, { 3:2, 1:1 }))
 
     # 1 new block from 3 cached blocks
     cache = updateCache(cache, window, hashesSize, 'h04', 39, testRetriever)
-    assertEquals(cache, Cache((1, 3, 3, 1),       ('h04', 'h03', 'h02'), 39, { 1:2, 3:2 }))
+    assertEquals(cache, Cache(      (1, 3, 3, 1,), ('h04', 'h03', 'h02'), 39, { 1:2, 3:2 }))
 
     # 1 new block from 4 cached blocks
     cache = updateCache(cache, window, hashesSize, 'h05', 40, testRetriever)
-    assertEquals(cache, Cache((2, 1, 3, 3, 1),    ('h05', 'h04', 'h03'), 40, { 2:1, 1:2, 3:2 }))
+    assertEquals(cache, Cache(   (2, 1, 3, 3, 1,), ('h05', 'h04', 'h03'), 40, { 2:1, 1:2, 3:2 }))
+
+    # 1 new block from 5 cached blocks - using a cache with wrong data to test that it's used
+    cache =             Cache(   (2, 4, 9, 7, 1,), ('h05', 'h04', 'h03'), 40, { 2:1, 4:1, 9:1, 7:1, 1:1 })
+    cache = updateCache(cache, window, hashesSize, 'h06', 41, testRetriever)
+    assertEquals(cache, Cache((0, 2, 4, 9, 7, 1,), ('h06', 'h05', 'h04'), 41, { 0:1, 2:1, 4:1, 9:1, 7:1, 1:1 }))
 
     # 1 new block from 5 cached blocks - last block of a period
+    cache =             Cache((   2, 1, 3, 3, 1,), ('h05', 'h04', 'h03'), 40, { 2:1, 1:2, 3:2 })
     cache = updateCache(cache, window, hashesSize, 'h06', 41, testRetriever)
-    assertEquals(cache, Cache((0, 2, 1, 3, 3, 1), ('h06', 'h05', 'h04'), 41, { 0:1, 2:1, 1:2, 3:2 }))
+    assertEquals(cache, Cache((0, 2, 1, 3, 3, 1,), ('h06', 'h05', 'h04'), 41, { 0:1, 2:1, 1:2, 3:2 }))
 
     # 1 new block from 6 cached blocks - first block of a period
     cache = updateCache(cache, window, hashesSize, 'h07', 42, testRetriever)
-    assertEquals(cache, Cache((9,),               ('h07',),              42, { 9:1 }))
+    assertEquals(cache, Cache(               (9,), ('h07',),              42, { 9:1 }))
 
     # 1 new block from 1 cached blocks - second block of a period
     cache = updateCache(cache, window, hashesSize, 'h08', 43, testRetriever)
-    assertEquals(cache, Cache((4, 9),             ('h08', 'h07'),        43, { 4:1, 9:1 }))
+    assertEquals(cache, Cache(            (4, 9,), ('h08', 'h07'),        43, { 4:1, 9:1 }))
 
     # 2 new blocks from 2 cached block
     cache = updateCache(cache, window, hashesSize, 'h10', 45, testRetriever)
-    assertEquals(cache, Cache((2, 3, 4, 9),       ('h10', 'h09', 'h08'), 45, { 2:1, 3:1, 4:1, 9:1 }))
+    assertEquals(cache, Cache(      (2, 3, 4, 9,), ('h10', 'h09', 'h08'), 45, { 2:1, 3:1, 4:1, 9:1 }))
 
     # 5 new blocks from 4 cached blocks - straight to the first 3 blocks of a new period
     cache = updateCache(cache, window, hashesSize, 'h15', 50, testRetriever)
-    assertEquals(cache, Cache((2, 3, 0),          ('h15', 'h14', 'h13'), 50, { 2:1, 3:1, 0:1 }))
+    assertEquals(cache, Cache(         (2, 3, 0,), ('h15', 'h14', 'h13'), 50, { 2:1, 3:1, 0:1 }))
 
     # 3 new blocks from 3 cached blocks
     cache = updateCache(cache, window, hashesSize, 'h18', 53, testRetriever)
-    assertEquals(cache, Cache((0, 9, 2, 2, 3, 0), ('h18', 'h17', 'h16'), 53, { 0:2, 9:1, 2:2, 3:1 }))
+    assertEquals(cache, Cache((0, 9, 2, 2, 3, 0,), ('h18', 'h17', 'h16'), 53, { 0:2, 9:1, 2:2, 3:1 }))
 
     # 4 new blocks from 6 cached blocks
     cache = updateCache(cache, window, hashesSize, 'h22', 57, testRetriever)
-    assertEquals(cache, Cache((4, 0, 4, 9),       ('h22', 'h21', 'h20'), 57, { 4:2, 0:1, 9:1 }))
+    assertEquals(cache, Cache(      (4, 0, 4, 9,), ('h22', 'h21', 'h20'), 57, { 4:2, 0:1, 9:1 }))
 
     # 2 new blocks from 4 cached blocks - the last 2 blocks have been orphaned
-    cache =             Cache((1, 7, 4, 9),       ('g22', 'g21', 'h20'), 57, { 1:1, 7:1, 4:1, 9:1 })
+    cache =             Cache(      (1, 7, 4, 9,), ('g22', 'g21', 'h20'), 57, { 1:1, 7:1, 4:1, 9:1 })
     cache = updateCache(cache, window, hashesSize, 'h24', 59, testRetriever)
-    assertEquals(cache, Cache((3, 2, 4, 0, 4, 9), ('h24', 'h23', 'h22'), 59, { 3:1, 2:1, 4:2, 0:1, 9:1 }))
+    assertEquals(cache, Cache((3, 2, 4, 0, 4, 9,), ('h24', 'h23', 'h22'), 59, { 3:1, 2:1, 4:2, 0:1, 9:1 }))
 
     # 1 new blocks from 6 cached blocks - same height but the last block has been orphaned
-    cache =             Cache((7, 2, 4, 0, 4, 9), ('g24', 'h23', 'h22'), 59, { 7:1, 2:1, 4:2, 0:1, 9:1 })
+    cache =             Cache((7, 2, 4, 0, 4, 9,), ('g24', 'h23', 'h22'), 59, { 7:1, 2:1, 4:2, 0:1, 9:1 })
     cache = updateCache(cache, window, hashesSize, 'h24', 59, testRetriever)
-    assertEquals(cache, Cache((3, 2, 4, 0, 4, 9), ('h24', 'h23', 'h22'), 59, { 3:1, 2:1, 4:2, 0:1, 9:1 }))
+    assertEquals(cache, Cache((3, 2, 4, 0, 4, 9,), ('h24', 'h23', 'h22'), 59, { 3:1, 2:1, 4:2, 0:1, 9:1 }))
 
     # 2 new blocks from 6 cached blocks - all 3 blocks in the cache have been orphaned
-    cache =             Cache((7, 0, 7, 2, 7, 8), ('g24', 'g23', 'g22'), 59, { 7:3, 0:1, 2:1, 8:1 })
+    cache =             Cache((7, 0, 7, 2, 7, 8,), ('g24', 'g23', 'g22'), 59, { 7:3, 0:1, 2:1, 8:1 })
     cache = updateCache(cache, window, hashesSize, 'h26', 61, testRetriever)
-    assertEquals(cache, Cache((8, 8),             ('h26', 'h25'),        61, { 8:2 }))
+    assertEquals(cache, Cache(            (8, 8,), ('h26', 'h25'),        61, { 8:2 }))
 
     # 2 new blocks from 2 cached blocks - all 2 blocks in the cache have been orphaned
-    cache =             Cache((7, 3),             ('g26', 'g25'),        61, { 7:1, 3:1 })
+    cache =             Cache(            (7, 3,), ('g26', 'g25'),        61, { 7:1, 3:1 })
     cache = updateCache(cache, window, hashesSize, 'h28', 63, testRetriever)
-    assertEquals(cache, Cache((8, 2, 8, 8),       ('h28', 'h27', 'h26'), 63, { 8:3, 2:1 }))
+    assertEquals(cache, Cache(      (8, 2, 8, 8,), ('h28', 'h27', 'h26'), 63, { 8:3, 2:1 }))
 
     # 10 new blocks from 4 cached blocks - skips a whole period
     cache = updateCache(cache, window, hashesSize, 'h38', 73, testRetriever)
-    assertEquals(cache, Cache((3, 4),             ('h38', 'h37'),        73, { 3:1, 4:1 }))
+    assertEquals(cache, Cache(            (3, 4,), ('h38', 'h37'),        73, { 3:1, 4:1 }))
 
     # 5 blocks backwards reorg from 2 cached blocks
-    cache =             Cache((2, 7),             ('g38', 'g37'),        73, { 2:1, 7:1 })
+    cache =             Cache(            (2, 7,), ('g38', 'g37'),        73, { 2:1, 7:1 })
     cache = updateCache(cache, window, hashesSize, 'h33', 68, testRetriever)
-    assertEquals(cache, Cache((2, 1, 0),          ('h33', 'h32', 'h31'), 68, { 2:1, 1:1, 0:1 }))
+    assertEquals(cache, Cache(         (2, 1, 0,), ('h33', 'h32', 'h31'), 68, { 2:1, 1:1, 0:1 }))
 
 def testAll():
     test_updateCache()
